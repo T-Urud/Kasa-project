426e711a16b09ab9c4d6cb6426a848ed04c66aa5
urud.theo@gmail.com <urud.theo@gmail.com>
2024-06-05 15:53:07 +0200

    changes on cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Slideshow from "./Slideshow";
 
 const Card = ({ project }) => {
+  const [titleLength, setTitleLength] = useState(false);
+
+  useEffect(() => {
+    if (project.title.length > 38) {
+      setTitleLength(true);
+    } else {
+      setTitleLength(false);
+    }
+  }, [project.title]);
+
   return (
     <li className="propertyContainer">
       <img src={project.cover} alt={project.title} />
-      <div className="propertyInfo">
+      <div
+        className="propertyInfo"
+        style={{ maxWidth: titleLength ? "60%" : "none" }}
+      >
         <p>{project.title}</p>
       </div>
     </li>
