99823a1f1b328a65c1588795622b6fc4f2a59333
urud.theo@gmail.com <urud.theo@gmail.com>
2024-06-12 14:19:57 +0200

    toggle function cretaed

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,11 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const About = () => {
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(null);
+
+  const toggleVisibility = (section) => {
+    setIsVisible(isVisible === section ? null : section);
+  };
 
   return (
     <div>
@@ -13,11 +17,71 @@ const About = () => {
         <div className="aboutContainer">
           <div className="alwaysShow">
             <p>Fiabilité</p>
-            <button onClick={() => setIsVisible(true)}>&#10096;</button>
+            <button onClick={() => toggleVisibility("fiabilite")}>
+              &#10096;
+            </button>
           </div>
           <div
             className="content"
-            style={{ display: isVisible ? "block" : "none" }}
+            style={{
+              display: isVisible === "fiabilite" ? "block" : "none",
+            }}
+          >
+            <p>
+              Les annonces postées sur Kasa garantissent une fiabilité totale.
+              Les photos sont conformes aux logements, et toutes les
+              informations sont régulièrement vérifiées par nos équipes.
+            </p>
+          </div>
+        </div>
+        <div className="aboutContainer">
+          <div className="alwaysShow">
+            <p>Respect</p>
+            <button onClick={() => toggleVisibility("respect")}>
+              &#10096;
+            </button>
+          </div>
+          <div
+            className="content"
+            style={{ display: isVisible === "respect" ? "block" : "none" }}
+          >
+            <p>
+              Les annonces postées sur Kasa garantissent une fiabilité totale.
+              Les photos sont conformes aux logements, et toutes les
+              informations sont régulièrement vérifiées par nos équipes.
+            </p>
+          </div>
+        </div>
+        <div className="aboutContainer">
+          <div className="alwaysShow">
+            <p>Service</p>
+            <button onClick={() => toggleVisibility("service")}>
+              &#10096;
+            </button>
+          </div>
+          <div
+            className="content"
+            style={{ display: isVisible === "service" ? "block" : "none" }}
+          >
+            <p>
+              Les annonces postées sur Kasa garantissent une fiabilité totale.
+              Les photos sont conformes aux logements, et toutes les
+              informations sont régulièrement vérifiées par nos équipes.
+            </p>
+          </div>
+        </div>
+        <div className="aboutContainer">
+          <div className="alwaysShow">
+            <p>Sécurité</p>
+            <button onClick={() => toggleVisibility("securite")}>
+              &#10096;
+            </button>
+          </div>
+          <div
+            className="content"
+            style={{
+              display: isVisible === "securite" ? "block" : "none",
+            }}
           >
             <p>
               Les annonces postées sur Kasa garantissent une fiabilité totale.
