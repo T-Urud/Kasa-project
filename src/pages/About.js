59273242fbe2e971421cf07faaa8dac81a57659b
urud.theo@gmail.com <urud.theo@gmail.com>
2024-06-12 12:13:32 +0200

    about page updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const About = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
     <div>
       <Header />
+      <div className="imgMottoContainer"></div>
+      <div className="aboutGeneralContainer">
+        <div className="aboutContainer">
+          <div className="alwaysShow">
+            <p>Fiabilité</p>
+            <button onClick={() => setIsVisible(true)}>&#10096;</button>
+          </div>
+          <div
+            className="content"
+            style={{ display: isVisible ? "block" : "none" }}
+          >
+            <p>
+              Les annonces postées sur Kasa garantissent une fiabilité totale.
+              Les photos sont conformes aux logements, et toutes les
+              informations sont régulièrement vérifiées par nos équipes.
+            </p>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
